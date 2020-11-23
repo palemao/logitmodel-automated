5557e7ad45710967968b54e7319cfb380ef0264a
palemao <74777355+palemao@users.noreply.github.com>
2020-11-23 04:48:33 +0000

    Update Automated Script.py

--- Automated Script.py
+++ Automated Script.py
@@ -9,7 +9,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegressionCV
-#from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 import statsmodels.api as sm
@@ -24,7 +23,6 @@ data2 = data2.dropna().reset_index(drop=True)
 '''
 #Checking Correlation between independent variables & Multi-collinearity with VIF test
 '''
-
 def heatmap(x_variables):
     corr = x_variables.corr()
     sns.heatmap(corr, cmap='RdBu')
@@ -54,9 +52,7 @@ def dummyvariables(dataset, column, attribute):  #creates dummy variables from c
 
 '''
 Define X_Variables models
-
 '''
-
 x_1 = ['CHAS', 'RM']
 x_2 = [ 'AGE', 'DIS']
 x_3 = ['DIS', 'RM', 'TAX', 'INDUS']
@@ -96,35 +92,34 @@ predictedvalues_list_test = [pd.DataFrame(model_list_std[i].predict_proba(x_data
 predictedvalues_table_test = [pd.merge(y_variable_test[i], predictedvalues_list_test[i], how='inner',
          left_index=True, right_index=True).drop(columns=['No']) for i in range(len(predictedvalues_list_test))]
     
-
-
 ######################################################################################################################
 
 '''
-Below I'm Using OVERSAMPLING to balnace the data. Use only if data is very unbalanced
+Below I'm Using OVERSAMPLING to balance the data. Use only if data is very unbalanced
 '''
-df_majority = data2[data2[y]==0]
-df_minority = data2[data2[y]==1]
+tester = [pd.merge(x_data_list[i], y_variable[i], how='inner',  left_index=True, right_index=True) for i in range(len(train_test))]
 
-over_sampled_y = resample(df_minority,
-                                 replace=True,     # sample with replacement
-                                 n_samples=len(df_majority),    # to match majority class
-                                 random_state=123)
+df_majority = [i[i[y]==0] for i in tester]
+df_minority = [i[i[y]==1] for i in tester]
 
-inputed_dataset = pd.concat([df_majority, over_sampled_y], ignore_index=True)
+over_sampled_y = [resample(df_minority[i],
+                              replace=True,     # sample with replacement
+                              n_samples=len(df_majority[i]),    # to match majority class
+                              random_state=123) for i in range(len(df_minority))]
+
+inputed_datasets = [pd.concat([df_majority[i], over_sampled_y[i]], ignore_index=True) for i in range(len(over_sampled_y))]
 
 '''
 # Define variables for inputed dataset
 '''
-y_inputed = inputed_dataset[y]
+y_inputed = [i[y] for i in inputed_datasets]
 
-x_inputed = [inputed_dataset[i] for i in x_list]
+x_inputed = [i.drop(columns=[y]) for i in inputed_datasets] 
 
-model_list_std_inputed = [LogisticRegressionCV(cv=5, max_iter=1000, solver='newton-cg', penalty='l2').fit(x_inputed[i], y_inputed) for i in range(len(x_inputed))]
+model_list_std_inputed = [LogisticRegressionCV(cv=5, max_iter=1000, solver='newton-cg', penalty='l2').fit(x_inputed[i], y_inputed[i]) for i in range(len(x_inputed))]
 
-predictedvalues_list_std_inputed = [pd.DataFrame(model_list_std_inputed[i].predict_proba(x_data_list[i]), columns=['No', 'Yes']) for i in range(len(x_data_list))]
+predictedvalues_list_test_ovr = [pd.DataFrame(model_list_std_inputed[i].predict_proba(x_data_list_test[i]), columns=['No', 'Yes']) for i in range(len(x_data_list_test))]
 
- 
 #ROC/AUC
 
 class ROC():
@@ -164,7 +159,9 @@ class ROC():
         return df2
 
 roc1_train = [ROC(y_variable[i] ,predictedvalues_list_std[i]['Yes']) for i in range(len(y_variable))]
-roc1_test = [ROC(y_variable_test[i] ,predictedvalues_list_test[i]['Yes']) for i in range(len(y_variable))] 
+roc1_test = [ROC(y_variable_test[i] ,predictedvalues_list_test[i]['Yes']) for i in range(len(y_variable))]
+roc1_test_ovr = [ROC(y_variable_test[i] , predictedvalues_list_test_ovr[i]['Yes']) for i in range(len(y_variable))]
+
 '''
 PLotting ROC Training & Testing
 '''
@@ -186,11 +183,11 @@ def plot_rocs(roc_output, title='unknown'):
     ax1.set_ylabel("True Positive Rate")
     ax1.set_title(title)
 
-#Training data ROC plot
 plot_rocs(roc1_train, title='Train')
 
-#Testing ROC  plot
 plot_rocs(roc1_test, title='Test')
+
+plot_rocs(roc1_test_ovr, title='Inputedmodel_Testdata')
 
 '''Comparing optimal threshold'''
 
@@ -212,13 +209,14 @@ def optthreshold_andCVscore(roc, x_train_data_list):
 
 optimal_train = optthreshold_andCVscore(roc1_train, x_data_list)
 optimal_test = optthreshold_andCVscore(roc1_test, x_data_list)  
+optimal_test_ovr = optthreshold_andCVscore(roc1_test_ovr, x_data_list)  
     
 '''
 Choosing any threshold you want
 '''
 list_of_tpr_tnr_train = [roc1_train[i].fpr_tnr_df() for i in range(len(roc1_train))]
 list_of_tpr_tnr_test = [roc1_test[i].fpr_tnr_df() for i in range(len(roc1_test))]
-
+list_of_tpr_tnr_test_ovr = [roc1_test_ovr[i].fpr_tnr_df() for i in range(len(roc1_test))]
 
 '''
 Coverting parameters to odds ratio and summarising
@@ -233,189 +231,7 @@ def odds_ratios(model_list):
     df1 = [pd.DataFrame(data=d1[i]) for i in range(len(d1))]
     return df1
 
-odds_ratio_table = odds_ratios(model_list_std) 
+odds_ratio_table = odds_ratios(model_list_std)
+odds_ratio_table_ovr = odds_ratios(model_list_std_inputed) 
 
 
-    
-
-    
-
- 
-
- 
-
- 
-
-'''
-
- 
-
- 
-
-'''
-
-#Log-Liklihood of models & Log-liklihood Test
-
- 
-
-'''
-
- 
-
- 
-
- 
-
- 
-
-def loglike(model_from_LogisticRegressionCV, x_inputs, y_actual):
-    predicted_prob = pd.DataFrame(model_from_LogisticRegressionCV.predict_proba(x_inputs), columns =['no', 'yes'])
-    predicted_prob['actual'] = y_actual
-    all_1s = predicted_prob[predicted_prob['actual']==1]
-    all_1s = all_1s.drop(columns=['no', 'actual'])
-    all_0s = predicted_prob[predicted_prob['actual']==0]
-    all_0s = all_0s.drop(columns=['yes', 'actual'])
-
- 
-
-    correct_prob = pd.concat([all_0s, all_1s], ignore_index=True)
-
-    correct_prob = correct_prob['no'].fillna(correct_prob['yes'])
-
-    correct_prob = correct_prob.rename('relevant_prob')
-
- 
-
-    log_like = np.log(correct_prob)
-
-    model_log_like= np.sum(log_like)
-
-    return(model_log_like)
-
- 
-
- 
-
-#loglike_model = loglike(model, x1, y)
-
- 
-
-#loglike_model1 = loglike(model1, x2, y)
-
- 
-
-#print('Restricted Model:' , loglike_model)
-
-#print('UnRestricted Model:' , loglike_model1)
-
- 
-
- 
-
- 
-
- 
-
- 
-
-def loglikelyhoodtest(log_likehood_restricted, log_likehood_unrestricted, dof, p=None):
-
- 
-
-p = 0.95   
-
- #dof = len(log_likehood_unrestricted)  
-
- test_stat = -2*(log_likehood_restricted - log_likehood_unrestricted)
-
-crit_value = chi2.ppf(p, dof)
-
-confirmedp = chi2.cdf(crit_value, dof)
-
-print("confirmed", confirmedp, "p-level")
-
- if test_stat < 0:
-
-  modulus_v =  test_stat*-1
-
-else:
-
-  modulus_v = test_stat
-
-print("test stat is", modulus_v)
-
-if modulus_v >= crit_value:
-
-    return("Unresticted model is statistically different to restricted model")
-
-else:
-
-    return("Unresticted model is NOT statistically different to restricted model")
-
- 
-
- 
-
- 
-
-#likelihoodtest = loglikelyhoodtest(loglike_model, loglike_model1, dof=3)
-
- 
-
-#print(loglikelyhoodtest(loglike_model, loglike_model1, dof=3))
-
- 
-
- 
-
- 
-
- 
-
- 
-
-from scipy.stats import norm
-
- 
-
-def logit_pvalue(model, x):
-
-    """ Calculate z-scores for scikit-learn LogisticRegression.
-
-    parameters:
-
-        model: fitted sklearn.linear_model.LogisticRegression with intercept and large C
-
-        x:     matrix on which the model was fit
-
-    This function uses asymtptics for maximum likelihood estimates.
-
-    """
-
-    p = model.predict_proba(x)
-
-    n = len(p)
-
-    m = len(model.coef_[0]) + 1
-
-    coefs = np.concatenate([model.intercept_, model.coef_[0]])
-
-    x_full = np.matrix(np.insert(np.array(x), 0, 1, axis = 1))
-
-    ans = np.zeros((m, m))
-
-    for i in range(n):
-
-        ans = ans + np.dot(np.transpose(x_full[i, :]), x_full[i, :]) * p[i,1] * p[i, 0]
-
-    vcov = np.linalg.inv(np.matrix(ans))
-
-    se = np.sqrt(np.diag(vcov))
-
-    t =  coefs/se 
-
-    p = (1 - norm.cdf(abs(t))) * 2
-
-    return p
-
-'''
